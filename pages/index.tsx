4f44c58843e3a308c703daae20dd87710f11aad1
Anas Aly Abdelhafiz <anabdelhafiz.c@stc.com.sa>
2025-07-10 11:58:30 +0300

    - done

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import { Projects } from "@/components/Projects";
 import { Contact } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
 import { Experience } from "@/components/Experience";
-import "./globals.css";
+// Removed import of "./globals.css" as it should be imported in app/layout.tsx
 
 export default function Home() {
   return (
