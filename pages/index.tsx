d436095afb306a03c59274187c905fef2a0a360c
Anas Aly Abdelhafiz <anabdelhafiz.c@stc.com.sa>
2025-07-10 11:45:36 +0300

    - handle styles issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import { Projects } from "@/components/Projects";
 import { Contact } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
 import { Experience } from "@/components/Experience";
-import "../app/globals.css";
+import "./globals.css";
 
 export default function Home() {
   return (
