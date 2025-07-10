b21973f6a598bb95ad2d892f0a78934f725f39bb
Anas Aly Abdelhafiz <anabdelhafiz.c@stc.com.sa>
2025-07-10 11:38:57 +0300

    - handle issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import { Projects } from "@/components/Projects";
 import { Contact } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
 import { Experience } from "@/components/Experience";
+import "../app/globals.css";
 
 export default function Home() {
   return (
@@ -17,11 +18,11 @@ export default function Home() {
         <Hero />
         <About />
         <Skills />
-        <Experience/>
+        <Experience />
         <Projects />
         <Contact />
       </main>
       <Footer />
     </div>
   );
-}
+} 
