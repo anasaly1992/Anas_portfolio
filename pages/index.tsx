feaaa5456ded5cb6af5c7096423dfc0904d9dd53
Anas Aly Abdelhafiz <anabdelhafiz.c@stc.com.sa>
2025-07-13 09:29:04 +0300

    - done

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ import { Projects } from "@/components/Projects";
 import { Contact } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
 import { Experience } from "@/components/Experience";
-import "../app/globals.css";
 
 export default function Home() {
   return (
